e7e45423c121911c22a1a8c036a1ab0ba9772e11
Jonathan Cai <caimjonathan@gmail.com>
2021-10-13 20:42:49 -0400

    feat: cleanup blog list page

--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -1,3 +1,5 @@
 export default function SectionContainer({ children }) {
-  return <div className="max-w-xl px-4 py-8 mx-auto sm:px-6 xl:max-w-3xl xl:px-0 ">{children}</div>
+  return (
+    <div className="max-w-xl px-4 pt-4 pb-8 mx-auto sm:px-6 xl:max-w-3xl xl:px-0 ">{children}</div>
+  )
 }
