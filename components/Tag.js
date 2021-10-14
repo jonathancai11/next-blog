e0df095346d818c7ee5c59e8a21e39b82ca6fc37
Jonathan Cai <caimjonathan@gmail.com>
2021-10-13 21:14:55 -0400

    feat: tag styling

--- components/Tag.js
+++ components/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400 border rounded px-2">
+      <a className="mr-3 py-0.5 text-sm font-medium lowercase bg-gray-200 hover:text-white hover:bg-gray-800 rounded px-2">
         {text.split(' ').join('-')}
       </a>
     </Link>
