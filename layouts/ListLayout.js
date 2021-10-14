894b53fe5f3b7f4b429938d5c46a630cb8a35ecc
Jonathan Cai <caimjonathan@gmail.com>
2021-10-13 20:49:36 -0400

    feat: minor style tags and posts page

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -19,7 +19,7 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   return (
     <>
       <div>
-        <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+        <div className="pt-4 pb-2 space-y-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {title}
           </h1>
